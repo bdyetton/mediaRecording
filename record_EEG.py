6633f20d42bda7cccb8168853b1394950598674a
andrewjsauer <andrewjohnsauer@gmail.com>
2018-10-20 14:05:05 -0700

    Changed thread to process

--- record_EEG.py
+++ record_EEG.py
@@ -3,11 +3,11 @@ from recordMuseOutputRecording import start_recording
 from playMediaSendMarkers import start_stream
 import argparse
 
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input_filename", help="Media file to play")
+    parser.add_argument("output_filename", help="Output csv file to save the recordings to")
+    args = parser.parse_args()
 
-parser = argparse.ArgumentParser()
-parser.add_argument("input_filename", help="Media file to play")
-parser.add_argument("output_filename", help="Output csv file to save the recordings to")
-args = parser.parse_args()
-
-Process(target=start_stream, args=(args.input_filename, )).start()
-Process(target=start_recording, args=(args.output_filename, )).start()
+    Process(target=start_stream, args=(args.input_filename,)).start()
+    Process(target=start_recording, args=(args.output_filename,)).start()
